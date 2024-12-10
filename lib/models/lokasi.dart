522786988cf8a735ccfcefdecf791c6bd76c9474
fikriszm34 <fikriszm34@gmail.com>
2024-12-10 20:51:49 +0000

    database lokasi (kecamatan)

---
+++ lib/models/lokasi.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+
+Random random = Random();
+
+class lokasi {
+  final String nama;
+
+  lokasi({
+    required this.nama
+  });
+}
+
+List<lokasi> listlokasi = [
+  lokasi(nama: "Cipanas"),
+  lokasi(nama: "Cianjur"),
+  lokasi(nama: "Bandung"),
+  lokasi(nama: "Jakarta"),
+];
