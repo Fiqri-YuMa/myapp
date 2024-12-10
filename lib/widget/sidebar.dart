96411a12fd48cd64da57f2cd7312d52335c8204f
fikriszm34 <fikriszm34@gmail.com>
2024-12-10 20:51:28 +0000

    menambah sidebar saat memilih kecamatan

---
+++ lib/widget/sidebar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/models/lokasi.dart';
+
+class Sidebar extends StatelessWidget {
+  final Function(String) lokasipilihan; 
+  const Sidebar({super.key, required this.lokasipilihan});
+
+  @override
+  Widget build(BuildContext context) {
+    List<lokasi> lok = listlokasi;
+
+    return Drawer(
+      child: Container(
+        color: const Color.fromARGB(255, 0, 57, 41),
+        child: ListView(
+        children: List.generate(
+          lok.length,
+          (index) => ListTile(
+            iconColor: Colors.white,
+            leading: const Icon(Icons.location_on),
+            title: Text(lok[index].nama,style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Roboto',
+              fontSize: 15,
+            ),),
+            onTap: () {
+              lokasipilihan(lok[index].nama);
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ),
+      ),
+    );
+  }
+}
