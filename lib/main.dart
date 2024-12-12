f6716a04540dcf9321b30d48b8cc17f95a749143
fikriszm34 <fikriszm34@gmail.com>
2024-12-12 14:40:09 +0000

    pisahkan

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,7 @@ import 'package:myapp/screen/favorit.dart';
 import 'package:myapp/screen/halaman_akun.dart';
 import 'package:myapp/screen/home.dart';
 import 'package:myapp/widget/sidebar.dart';
-
-class ExploreCianjur extends StatelessWidget {
-  const ExploreCianjur({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
-    );
-  }
-}
-
-void main() {
-  runApp(const ExploreCianjur());
-}
+import 'package:myapp/screen/app_bar.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -30,8 +15,20 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   int _currentIndex = 0;
-  int halaman = 0;
   String posisi = "Cipanas";
+
+  // Callback untuk menerima lokasi dari Sidebar
+  void updatePosisi(String newPosisi) {
+    setState(() {
+      posisi = newPosisi;
+    });
+  }
+
+  void onTappedBar(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   List<Widget> _getChildren() {
     return [
@@ -41,130 +38,16 @@ class _MainPageState extends State<MainPage> {
     ];
   }
 
-  void onTappedBar(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  void updatePosisi(String newPosisi) {
-    setState(() {
-      posisi = newPosisi; // Update posisi berdasarkan pilihan dari Sidebar
-    });
-  }
-
-  AppBar _buildAppBar() {
-    if (_currentIndex == 0) {
-      return AppBar(
-        flexibleSpace: Container(
-          height: 100,
-          margin: const EdgeInsets.only(left: 20),
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('image/logo.png'),
-              fit: BoxFit.none,
-              scale: 10.0,
-              alignment: Alignment.centerLeft,
-            ),
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 0, 28, 20),
-        actions: [
-          Container(
-            padding: const EdgeInsets.only(top: 12, bottom: 12),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(30)),
-              child: Container(
-                color: const Color.fromARGB(255, 0, 16, 12),
-                padding: const EdgeInsets.only(right: 20),
-                child: Row(
-                  children: [
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      padding:
-                          const EdgeInsets.only(right: 5, top: 1, left: 10),
-                      child: const Icon(
-                        Icons.location_on,
-                        size: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(posisi, style: const TextStyle(color: Colors.white)),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          IconButton(
-            onPressed: () {
-              _scaffoldKey.currentState?.openEndDrawer();
-            },
-            icon: const Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-          ),
-        ],
-        automaticallyImplyLeading: false,
-      );
-    } else if (_currentIndex == 2) {
-      return AppBar(
-        leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.arrow_back,
-              color: Colors.white,
-            )),
-        title: const Text(
-          "Akun",
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-        backgroundColor: const Color.fromARGB(255, 0, 28, 20),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {
-              _scaffoldKey.currentState?.openEndDrawer();
-            },
-            icon: const Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      );
-    } else {
-      return AppBar(
-        backgroundColor: const Color.fromARGB(255, 0, 28, 20),
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            // Tambahkan fungsi untuk kembali di sini
-          },
-        ),
-        title: const Text('Favorit', style: TextStyle(color: Colors.white)),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search, color: Colors.white),
-            onPressed: () {
-              _scaffoldKey.currentState?.openEndDrawer();
-              // Tambahkan fungsi pencarian di sini
-            },
-          ),
-        ],
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      endDrawer: Sidebar(
-        lokasipilihan: updatePosisi,
-      ),
-      appBar: _buildAppBar(),
+      endDrawer: Sidebar(onLokasiSelected: updatePosisi), // Pass callback
+      appBar: CustomAppBar(
+        posisi: posisi,
+        currentIndex: _currentIndex,
+        scaffoldKey: _scaffoldKey,
+      ), // Gunakan CustomAppBar
       body: IndexedStack(
         index: _currentIndex,
         children: _getChildren(),
